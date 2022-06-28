2d51d21e8f7c78e31c6414dc1ab77cb8921cf800
cholis04 <korunkolis04@gmail.com>
2022-06-28 09:58:50 +0700

    Added Highlight Keyword Text

--- src/components/BoxNoteCard/index.js
+++ src/components/BoxNoteCard/index.js
@@ -1,12 +1,27 @@
+import { useContext } from 'react';
+
+import { NotesContext } from '../../context/NotesContext';
+
 import { showFormattedDate } from '../../utils/showFormattedDate';
+import MarkText from '../MarkText';
 
 function BoxNoteCard({ notes, moveNote, deleteNote }) {
+  const { keyword } = useContext(NotesContext);
+
+  const regExpKeyword = new RegExp(keyword, 'gi');
+
   return (
     <div className="box-card">
       {notes.map((note) => {
         return (
           <article key={note.id}>
-            <h2>{note.title}</h2>
+            <h2>
+              <MarkText
+                keyword={keyword}
+                regExpKeyword={regExpKeyword}
+                text={note.title}
+              />
+            </h2>
             <p className="date-info">
               <span>[Icon]</span>{' '}
               <time dateTime="2015-08-17 20:22">
@@ -14,7 +29,13 @@ function BoxNoteCard({ notes, moveNote, deleteNote }) {
               </time>
             </p>
             <hr />
-            <p>{note.body}</p>
+            <p>
+              <MarkText
+                keyword={keyword}
+                regExpKeyword={regExpKeyword}
+                text={note.body}
+              />
+            </p>
             <hr />
             <div className="action">
               <button onClick={() => moveNote(note.id)}>
---
+++ src/components/MarkText/index.js
@@ -0,0 +1,31 @@
+function MarkText({ text, keyword, regExpKeyword }) {
+  if (keyword !== '') {
+    const splitterText = '___{$#(!VMV!)#$}___';
+
+    // Replacement with string
+    const replace = text.replaceAll(
+      regExpKeyword,
+      `${splitterText}$&${splitterText}`,
+    );
+
+    const split = replace.split(splitterText);
+
+    return (
+      <>
+        {split.map((word, index) => {
+          if (word.toLowerCase() === keyword.toLowerCase())
+            return (
+              <mark style={{ backgroundColor: 'yellow' }} key={index}>
+                {word}
+              </mark>
+            );
+          return word;
+        })}
+      </>
+    );
+  }
+
+  return <>{text}</>;
+}
+
+export default MarkText;
