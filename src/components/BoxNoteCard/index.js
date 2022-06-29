e9a9cd4363020d6d19008bf2f625a38c0c777f86
cholis04 <korunkolis04@gmail.com>
2022-06-29 22:02:54 +0700

    Implement Masonry Layout to Note List

--- src/components/BoxNoteCard/index.js
+++ src/components/BoxNoteCard/index.js
@@ -1,14 +1,27 @@
 import React from 'react';
+import Masonry from 'react-masonry-css';
 
 import NoteCard from '../NoteCard';
 
+import styles from './index.module.css';
+
+const breakpointColumnsObj = {
+  default: 3,
+  1024: 2,
+  640: 1,
+};
+
 function BoxNoteCard({ notes }) {
   return (
-    <div className="box-card">
+    <Masonry
+      breakpointCols={breakpointColumnsObj}
+      className={styles.masonryGrid}
+      columnClassName={styles.masonryColumn}
+    >
       {notes.map((note) => {
         return <NoteCard key={note.id} note={note} />;
       })}
-    </div>
+    </Masonry>
   );
 }
 
