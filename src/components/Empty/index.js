30833cb363e279f489ae1649b8bd0c3ed7e2e1b3
cholis04 <korunkolis04@gmail.com>
2022-06-29 16:00:27 +0700

    Added style Empty Data

--- src/components/Empty/index.js
+++ src/components/Empty/index.js
@@ -1,16 +1,20 @@
 import EmptyInboxDesktop from '../../assets/images/Empty_Inbox_Outline_Desktop.png';
 import EmptyInboxMobile from '../../assets/images/Empty_Inbox_Outline_Mobile.png';
 
+import styles from './index.module.css';
+
 function Empty({ openModal }) {
   return (
-    <div className="box-empty">
+    <div className={styles.boxEmpty}>
       <picture>
         <source media="(min-width:768px)" srcSet={EmptyInboxDesktop} />
         <img src={EmptyInboxMobile} alt="" />
       </picture>
-      <p>
+      <p className={styles.text}>
         Catatan singkat tidak ditemukan,{' '}
-        <button onClick={openModal}>Buat sekarang!</button>
+        <button onClick={openModal} className={styles.btnAdd}>
+          Buat sekarang!
+        </button>
       </p>
     </div>
   );
--- src/components/TabPanel/index.js
+++ src/components/TabPanel/index.js
@@ -6,6 +6,8 @@ import { PanelContext } from '../../context/PanelContext';
 import BoxNoteCard from '../BoxNoteCard';
 import Empty from '../Empty';
 
+import styles from './index.module.css';
+
 function TabPanel({ panelId, panelName, panelLabel, notes }) {
   const { panel } = useContext(PanelContext);
   const { openModal } = useContext(ModalContext);
@@ -17,6 +19,7 @@ function TabPanel({ panelId, panelName, panelLabel, notes }) {
       tabIndex="0"
       aria-labelledby={panelLabel}
       hidden={panel !== panelName}
+      className={styles.tabpanel}
     >
       {notes.length >= 1 ? (
         <BoxNoteCard notes={notes} />
