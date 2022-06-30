ce2adb7ee0d7bd4f765bd7e84e356dd374dd5364
cholis04 <korunkolis04@gmail.com>
2022-06-30 18:22:50 +0700

    Added Style Not Found Page

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -1,11 +1,26 @@
 import { useEffect } from 'react';
+import { Link } from 'react-router-dom';
+
+import styles from '../styles/notfound.module.css';
 
 function NotFound() {
   useEffect(() => {
-    document.title = '404 | Page not found';
+    document.title = 'Halaman tidak ditemukan!';
   });
 
-  return <div>NotFound</div>;
+  return (
+    <main className={styles.main}>
+      <p className={styles.notfound}>404</p>
+      <h1 className={styles.title}>Ups, terjadi kesalahan!</h1>
+      <p className={styles.paragraph}>
+        Halaman yang anda cari tidak ditemukan. Silahkan kembali ke{' '}
+        <Link className={styles.link} to="/">
+          halaman utama
+        </Link>
+        .
+      </p>
+    </main>
+  );
 }
 
 export default NotFound;
