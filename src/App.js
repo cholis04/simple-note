4253f7cecfc692083f097dffc32267495f304bcb
cholis04 <korunkolis04@gmail.com>
2022-09-26 09:44:43 +0700

    Added Note Route

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import NotesContextProvider from './context/NotesContext';
 
 import Archive from './pages/Archive';
 import Home from './pages/Home';
+import Note from './pages/Note';
 import NotFound from './pages/NotFound';
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/arsip" element={<Archive />} />
+        <Route path="/catatan/:id" element={<Note />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
     </NotesContextProvider>
