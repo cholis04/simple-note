27ec8a61a66121e663b3ff49e6ed50a46eb18311
cholis04 <korunkolis04@gmail.com>
2022-09-26 17:42:24 +0700

    Added Info Date Component

---
+++ src/elements/InfoDate.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { ClockIcon } from '@heroicons/react/solid';
+
+import { formattedAttributeTime } from '../utils/formattedAttributeTime';
+import { showFormattedDate } from '../utils/showFormattedDate';
+
+import styles from './InfoDate.module.css';
+
+const InfoDate = ({ time }) => {
+  return (
+    <div className={styles.wrapper}>
+      <ClockIcon className={styles.iconTime} />{' '}
+      <time dateTime={formattedAttributeTime(time)}>
+        {showFormattedDate(time)}
+      </time>
+    </div>
+  );
+};
+
+InfoDate.propTypes = {
+  time: PropTypes.string.isRequired,
+};
+
+export default InfoDate;
