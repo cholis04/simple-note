3720919c3d016720690518f2da4beb12507fc6af
cholis04 <korunkolis04@gmail.com>
2022-09-26 12:17:57 +0700

    Added Style Note Page

--- src/pages/Note.js
+++ src/pages/Note.js
@@ -1,5 +1,5 @@
 import { useContext, useEffect } from 'react';
-import { TrashIcon } from '@heroicons/react/solid';
+import { ClockIcon, TrashIcon } from '@heroicons/react/solid';
 import { useParams } from 'react-router-dom';
 
 import { NotesContext } from '../context/NotesContext';
@@ -9,6 +9,8 @@ import Footer from '../blocks/Footer';
 import NotFound from '../blocks/NotFound';
 
 import styles from '../styles/pages/Note.module.css';
+import { formattedAttributeTime } from '../utils/formattedAttributeTime';
+import { showFormattedDate } from '../utils/showFormattedDate';
 
 function Note() {
   const { id } = useParams();
@@ -21,6 +23,11 @@ function Note() {
     if (note !== undefined) document.title = `Catatan - ${note?.title}`;
   });
 
+  const countWords = (str) => {
+    const arr = str.split(' ');
+    return arr.filter((word) => word !== '').length;
+  };
+
   // Render NofFound when note is undefinded
   if (note === undefined) {
     return <NotFound />;
@@ -31,24 +38,43 @@ function Note() {
     <>
       <Header />
       <main className={styles.main}>
-        <section className={styles.main__detail}>
-          <div className={styles.main__detailWrapper}>
-            <h1>{note.title}</h1>
-            <div className={styles.action}>
-              <button
-                onClick={() => moveNote(note.id)}
-                className={styles.btnMove}
-              >
-                {note.archived ? '← Aktifkan' : 'Arsipkan →'}
-              </button>
+        {/* Note Detail */}
+        <section className={styles.detail}>
+          <div className={styles.detail__action}>
+            <button
+              onClick={() => moveNote(note.id)}
+              className={styles.btnMove}
+            >
+              {note.archived ? '← Aktifkan' : 'Arsipkan →'}
+            </button>
 
-              <button
-                onClick={() => deleteNote(note.id)}
-                className={styles.btnDelete}
-              >
-                <TrashIcon className={styles.iconBtn} /> Hapus
-              </button>
+            <button
+              onClick={() => deleteNote(note.id)}
+              className={styles.btnDelete}
+            >
+              <TrashIcon className={styles.iconBtn} /> Hapus
+            </button>
+          </div>
+          <div className={styles.detail__info}>
+            <div className={styles.detail__infoDate}>
+              <ClockIcon className={styles.iconTime} />{' '}
+              <time dateTime={formattedAttributeTime(note.createdAt)}>
+                {showFormattedDate(note.createdAt)}
+              </time>
             </div>
+            <p className={styles.detail__status}>
+              Status :{' '}
+              <span className={styles.detail__statusLabel}>
+                {note.archived ? 'Arsip' : 'Aktif'}
+              </span>
+            </p>
+          </div>
+          <h1 className={styles.titleNote}>{note.title}</h1>
+          <p className={styles.bodyNote}>{note.body}</p>
+          <div className={styles.detail__info}>
+            <p className={styles.detail__infoWordCount}>
+              Jumlah Kata : <strong>{countWords(note.body)}</strong>
+            </p>
           </div>
         </section>
       </main>
