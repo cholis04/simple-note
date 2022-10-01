5e21cfeecf0d80c66c6e459b77d0629364087a64
cholis04 <korunkolis04@gmail.com>
2022-10-02 06:59:25 +0700

    Reorganize css file

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 
-import './styles/global.css';
+import './styles/main.css';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
