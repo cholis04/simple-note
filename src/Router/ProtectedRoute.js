75af339c7fac3f2415aa7d9e0dd6fed122bc5b7a
cholis04 <korunkolis04@gmail.com>
2022-10-17 16:06:36 +0700

    Move loading into components

--- src/Router/ProtectedRoute.js
+++ src/Router/ProtectedRoute.js
@@ -2,7 +2,7 @@ import { Navigate, Outlet } from 'react-router-dom';
 
 import useAuth from '../hooks/useAuth';
 
-import Loading from '../blocks/Loading';
+import Loading from '../components/Loading';
 
 const ProtectedRoute = () => {
   const { user, loading } = useAuth();
--- src/Router/PublicRoute.js
+++ src/Router/PublicRoute.js
@@ -2,7 +2,7 @@ import { Navigate, Outlet } from 'react-router-dom';
 
 import useAuth from '../hooks/useAuth';
 
-import Loading from '../blocks/Loading';
+import Loading from '../components/Loading';
 
 const PublicRoute = () => {
   const { user, loading } = useAuth();
